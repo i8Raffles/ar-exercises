2e9895f91120c98a1c8a5fdfd69aa63faeb0cfb8
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-06-26 19:59:15 -0400

    complete exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jimmy", last_name: "Kimmel", hourly_rate: 50)
+@store1.employees.create(first_name: "Harry", last_name: "Kane", hourly_rate: 45)
+
+
+@store2.employees.create(first_name: "Ruben", last_name: "Neves", hourly_rate: 55)
+@store2.employees.create(first_name: "Jamie", last_name: "Oliver", hourly_rate: 65)
+@store2.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 70)
+
+
+puts "Employees created successfully!"
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter store name: "
+store_name = gets.chomp
+
+# new_store = Store.new(name: store_name, annual_revenue: 728832, mens_apparel: true, womens_apparel: true)
+new_store = Store.create(name: store_name)
+
+if !new_store.save
+  puts "Errors:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
